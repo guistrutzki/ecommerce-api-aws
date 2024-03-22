0985dde87e4ca14f1696e6ed4c658f3c354aa0ad
Guilherme Strutzki <guilherme.strutzki@compasso.com.br>
2024-03-21 23:51:49 -0300

    feat: add productsFetch implementations

--- lambda/products/productsFetchFunction.ts
+++ lambda/products/productsFetchFunction.ts
@@ -3,6 +3,13 @@ import {
   APIGatewayProxyResult,
   Context,
 } from 'aws-lambda';
+import { DynamoDB } from 'aws-sdk';
+import { ProductRepository } from '/opt/nodejs/productsLayer';
+
+const productsDdb = process.env.PRODUCTS_DDB ?? '';
+const ddbClient = new DynamoDB.DocumentClient();
+
+const productRepository = new ProductRepository(ddbClient, productsDdb);
 
 export async function handler(
   event: APIGatewayProxyEvent,
@@ -18,25 +25,34 @@ export async function handler(
 
   if (event.resource === '/products') {
     if (method === 'GET') {
-      console.log('GET');
+      console.log('GET /products');
+      const products = await productRepository.getAllProducts();
 
       return {
         statusCode: 200,
-        body: JSON.stringify({
-          message: 'GET Products - OK',
-        }),
+        body: JSON.stringify(products),
       };
     }
   } else if (event.resource === '/products/{id}') {
     const productId = event.pathParameters?.id ?? '';
     console.log(`GET /products/${productId}`);
 
-    return {
-      statusCode: 200,
-      body: JSON.stringify({
-        message: `GET /products/${productId}`,
-      }),
-    };
+    try {
+      const product = await productRepository.getProductById(productId);
+
+      return {
+        statusCode: 200,
+        body: JSON.stringify(product),
+      };
+    } catch (error) {
+      console.error((<Error>error).message);
+      return {
+        statusCode: 404,
+        body: JSON.stringify({
+          message: (<Error>error).message,
+        }),
+      };
+    }
   }
 
   return {
